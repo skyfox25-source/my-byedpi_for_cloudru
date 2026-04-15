c47c6b3aef2bf875786b985b980384a6250a45c3
skyfox25-source <skyfox25@gmail.com>
2026-04-15 17:38:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
 
-# Настройки (укажи свои переменные)
-REPO_URL="https://github.com/skyfor2000/my_byedpi_for_cloudhu/releases/latest/download/" # адрес загрузки
-FILE_NAME="my-byedpi-for-cloudhu.zip" # имя загружаемого архива
-INSTALL_DIR="/opt/my-byedpi-for-cloudhu" # каталог установки программы
-SYSTEMD_SERVICE_FILE="${INSTALL_DIR}/my-byedpi_for_cloudru.service" # файл службы systemd
+# Настройки (уже под твой репозиторий)
+REPO_URL="https://githubusercontent.com"
+FILE_NAME="ciadpi-x86_64"
+INSTALL_DIR="/usr/local/bin"
 
-# Сообщение начала процесса установки
-echo "Установка ByeDPI для CloudRu..."
+echo "--- Установка ByeDPI для Cloud.ru ---"
 
-# Загрузка архива
-wget "${REPO_URL}${FILE_NAME}" -P "$INSTALL_DIR"
+# 1. Скачивание файла
+sudo wget -O $INSTALL_DIR/$FILE_NAME "$REPO_URL/$FILE_NAME"
+sudo chmod +x $INSTALL_DIR/$FILE_NAME
 
-# Распаковка архива
-unzip -o "$INSTALL_DIR/$FILE_NAME" -d "$INSTALL_DIR"
-rm -rf "$INSTALL_DIR/$FILE_NAME" # удаление скачанного архива после распаковки
-
-# Создание файла службы systemd
-cat << EOF > "$SYSTEMD_SERVICE_FILE"
+# 2. Создание сервиса (-d 1 -m 1, как мы подобрали)
+sudo bash -c "cat <<EOT > /etc/systemd/system/byedpi.service
 [Unit]
-Description=MyByedpiForCloudRu service
+Description=ByeDPI Service
 After=network.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/python3 ${INSTALL_DIR}/main.py
+User=root
+ExecStart=$INSTALL_DIR/$FILE_NAME -d 1 -m 1 -p 1080
 Restart=always
 RestartSec=5
 
 [Install]
 WantedBy=multi-user.target
-EOF
+EOT"
 
-# Перезагружаем демон systemd и активируем новую службу
+# 3. Запуск
 sudo systemctl daemon-reload
-sudo systemctl enable my-byedpi_for_cloudru.service
-sudo systemctl start my-byedpi_for_cloudru.service
-sudo systemctl restart my-byedpi_for_cloudru.service
+sudo systemctl enable byedpi
+sudo systemctl restart byedpi
 
-# Завершение установки
-echo "... Готово!"
-echo "Проверка статуса: sudo systemctl status my-byedpi_for_cloudru.service."
+echo "--- Готово! Проверь статус: sudo systemctl status byedpi ---"
